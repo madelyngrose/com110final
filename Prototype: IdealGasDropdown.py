78d1c58b2d294858c93dbfb5b067c01658bc20d3
madelyngrose <135168218+madelyngrose@users.noreply.github.com>
2023-12-08 13:51:19 -0500

    Update Prototype: IdealGasDropdown.py

--- Prototype: IdealGasDropdown.py
+++ Prototype: IdealGasDropdown.py
@@ -3,22 +3,22 @@ from tkinter import *
 class IdealGasLaw:
     def __init__(self):
         self.root = Tk()
-        self.root.geometry("2000x2000")
-        self.root.title("Ideal Gas Law Calculator")
-        self.options = ["Pressure", "Volume", "Moles", "Temperature"]
+        self.root.geometry("2000x2000") #Sets window size
+        self.root.title("Ideal Gas Law Calculator") #Titles window
+        self.options = ["Pressure", "Volume", "Moles", "Temperature"] #Provides options for the initial dropdown menu
         self.selection = StringVar()
-        self.selection.set("What value would you like to find?")
+        self.selection.set("What value would you like to find?") #Initial prompt for user
         self.drop = OptionMenu(self.root, self.selection, *self.options)
         self.drop.pack()
-        self.button1 = Button(self.root, text="Select", command=self.show)
+        self.button1 = Button(self.root, text="Select", command=self.show) #Button that confirms the user's choice from the dropdown menu
         self.button1.pack()
         self.label = Label(self.root, text=" ")
         self.label.pack()
-        self.user_input_values = {}
+        self.user_input_values = {} #Creates a dictionary that the user values will be stored in
         self.root.mainloop()
 
     def user_input(self, variable):
-        label = Label(self.root, text=variable)
+        label = Label(self.root, text=variable) #Provides labels based on what values the user should have, given what they want to solve
         label.pack()
         textbox = Entry(self.root)
         textbox.pack()
@@ -29,18 +29,19 @@ class IdealGasLaw:
 
         if selected not in self.user_input_values:
             if selected == "Pressure":
-                variables = ["Volume", "Moles", "Temperature"]
+                variables = ["Volume in Liters", "Number of Moles", "Temperature in Kelvin"]
             elif selected == "Volume":
-                variables = ["Pressure", "Moles", "Temperature"]
+                variables = ["Pressure in Bars", "Number of Moles", "Temperature in Kelvin"]
             elif selected == "Moles":
-                variables = ["Pressure", "Volume", "Temperature"]
+                variables = ["Pressure in Bars", "Volume in Liters", "Temperature in Kelvin"]
             elif selected == "Temperature":
-                variables = ["Pressure", "Moles", "Volume"]
+                variables = ["Pressure in Bars", "Number of Moles", "Volume in Liters"]
 
             self.user_input_values[selected] = [self.user_input(f"Enter {param}") for param in variables]
 
-        values = [float(entry.get()) for entry in self.user_input_values[selected]]
-
+        values = [float(entry.get()) for entry in self.user_input_values[selected]] #Obtains the values input by the user
+        
+#Solves for the selected variable 
         if selected == "Pressure":
             volume, moles, temp = values
             pressure = (moles * 0.0831446 * temp) / volume
@@ -61,7 +62,7 @@ class IdealGasLaw:
             temp = (pressure * volume) / (0.0831446 * moles)
             output = ("The temperature is",temp,"Kelvin")
 
-        self.label.config(text=output)
+        self.label.config(text=output) Prints the output of the variable of interest
 
 IdealGasLaw()
 
