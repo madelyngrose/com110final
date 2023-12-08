07e65d390d33ef76bc4a2fb94f1ef45f98ef8ca6
madelyngrose <135168218+madelyngrose@users.noreply.github.com>
2023-12-08 13:23:05 -0500

    Update Prototype: IdealGasDropdown.py

--- Prototype: IdealGasDropdown.py
+++ Prototype: IdealGasDropdown.py
@@ -3,45 +3,44 @@ from tkinter import *
 class IdealGasLaw:
     def __init__(self):
         self.root = Tk()
+        self.root.geometry("400x300")
         self.root.title("Ideal Gas Law Calculator")
-        self.root.geometry("2000x2000") #Set window size
-        self.options = ["Pressure", "Volume", "Moles", "Temperature"] #Variable options for the dropdown menu
+        self.options = ["Pressure", "Volume", "Moles", "Temperature"]
         self.selection = StringVar()
-        self.selection.set("What value would you like to find?") #Starting selected variable that prompts the user to select a variable
+        self.selection.set("What value would you like to find?")
         self.drop = OptionMenu(self.root, self.selection, *self.options)
         self.drop.pack()
-        self.submission = Button(self.root, text="Select", command=self.show) #Creates a button for the user to confirm their selection choice
-        self.submission.pack()
+        self.button1 = Button(self.root, text="Select", command=self.show)
+        self.button1.pack()
         self.label = Label(self.root, text=" ")
         self.label.pack()
-        self.user_input_values = {} #Creates a dictionary to store the user inputs (utilized in line 29)
+        self.user_input_values = {}
         self.root.mainloop()
 
     def user_input(self, variable):
-        label = Label(self.root, text=variable) #Will label each of the text boxes according to the known variables
+        label = Label(self.root, text=variable)
         label.pack()
-        textbox = Entry(self.root) #Creates text box for users to import their known variables
+        textbox = Entry(self.root)
         textbox.pack()
         return textbox
 
     def show(self):
         selected = self.selection.get()
 
-        if selected not in self.user_input_values: #If what is selected is not in the dictionary (empty dictionary, so this should apply to everything)
-            if selected == "Pressure": #Selected dropdown variable
-                variables = ["Volume in Liters", "Number of Moles", "Temperature in Kelvin"] #Prompts for known user variables
+        if selected not in self.user_input_values:
+            if selected == "Pressure":
+                variables = ["Volume", "Moles", "Temperature"]
             elif selected == "Volume":
-                variables = ["Pressure in Bars", "Number of Moles", "Temperaturein Kelvin"]
+                variables = ["Pressure", "Moles", "Temperature"]
             elif selected == "Moles":
-                variables = ["Pressure in Bars", "Volume in Liters", "Temperature in Kelvin"]
+                variables = ["Pressure", "Volume", "Temperature"]
             elif selected == "Temperature":
-                variables = ["Pressure in Bars", "Number of Moles", "Volume in Liters"]
+                variables = ["Pressure", "Moles", "Volume"]
 
-            self.user_input_values[selected] = [self.user_input("Enter", param) for param in variables]
+            self.user_input_values[selected] = [self.user_input(f"Enter {param}") for param in variables]
 
-        values = [float(entry.get()) for entry in self.user_input_values[selected]] #Retrieve entry
+        values = [float(entry.get()) for entry in self.user_input_values[selected]]
 
-        #Calculate the selected variable using the ideal gas law
         if selected == "Pressure":
             volume, moles, temp = values
             pressure = (moles * 0.0831446 * temp) / volume
